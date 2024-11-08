10701c2289a2f00702af473ed4a24b0ace32e0b3
Ranjinie-Souria <ranjinie.souria@gmail.com>
2024-11-08 12:38:26 +0100

    Controllers

---
+++ src/main/java/com/party/Party/controller/ItemController.java
@@ -0,0 +1,50 @@
+package com.party.Party.controller;
+
+import com.party.Party.dto.ItemDto;
+import com.party.Party.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<ItemDto>> findAll(@RequestParam(defaultValue = "0") int page,
+                                                 @RequestParam(defaultValue = "5") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(itemService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemDto> findById(@PathVariable Long id) {
+        ItemDto itemDto = itemService.findById(id);
+        return itemDto != null ? ResponseEntity.ok(itemDto) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemDto> save(@RequestBody ItemDto itemDto) {
+        ItemDto createdItem = itemService.save(itemDto);
+        return ResponseEntity.ok(createdItem);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemDto> update(@PathVariable Long id, @RequestBody ItemDto itemDto) {
+        ItemDto updatedItem = itemService.update(id, itemDto);
+        return updatedItem != null ? ResponseEntity.ok(updatedItem) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        itemService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
